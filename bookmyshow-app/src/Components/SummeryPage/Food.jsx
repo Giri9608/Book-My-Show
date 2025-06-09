5450a35f2d84282fb25087f8674cbf111bd9dfe0
Giri9608 <129484657+Giri9608@users.noreply.github.com>
2025-06-09 18:21:25 +0530

    Update Food.jsx

--- bookmyshow-app/src/Components/SummeryPage/Food.jsx
+++ bookmyshow-app/src/Components/SummeryPage/Food.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFood, storeSelectedFood } from '../../Redux/food/actions';
-import styles from "../Styling/Food.module.css";
+import styles from '../Styling/Food.module.css';
 import FoodCard from './FoodCard';
-
 
 const init = {
     all: true,
@@ -11,93 +10,144 @@ const init = {
     snacks: false,
     beverages: false,
     popcorn: false,
-}
+};
+
 const Food = () => {
-    const dispatch = useDispatch()
-    const [filteredFood, setFliteredFood] = React.useState([])
-    const foods = useSelector(state => state.food.foods)
-    const [active, setActive] = React.useState(init)
-    const [selectedFood, setSelectedFood]= React.useState([])
+    const dispatch = useDispatch();
+    const foods = useSelector((state) => state.food.foods);
+    const [filteredFood, setFilteredFood] = useState([]);
+    const [active, setActive] = useState(init);
+    const [selectedFood, setSelectedFood] = useState([]);
 
-    React.useEffect(() => {
-        dispatch(getFood())
-        setFliteredFood(foods)
-        handleFilter("All")
-    }, [])
+    useEffect(() => {
+        dispatch(getFood());
+        handleFilter('ALL');
+    }, [dispatch, handleFilter]);
 
-    React.useEffect(() => {
-        setFliteredFood(foods)
-    }, [foods])
+    useEffect(() => {
+        setFilteredFood(foods);
+    }, [foods]);
+
+    useEffect(() => {
+        const temp = filteredFood.filter((item) => item.count > 0);
+        setSelectedFood(temp);
+        dispatch(storeSelectedFood(temp));
+    }, [filteredFood, dispatch]);
 
     const handleFilter = (text) => {
-        if (text === "CO") {
-            const updated = foods.filter(item => item.is_combo === true)
-            setFliteredFood(updated)
-            setActive({...active, combo: true, all:false, snacks:false, beverages:false, popcorn:false})
-        }else if (text === "SN") {
-            const updated = foods.filter(item => item.is_coke === true)
-            setFliteredFood(updated)
-            setActive({...active, combo: false, all:false, snacks:true, beverages:false, popcorn:false})
-        }else if (text === "BE") {
-            const updated = foods.filter(item => item.is_combo === true)
-            setFliteredFood(updated)
-            setActive({...active, combo: false, all:false, snacks:false, beverages:true, popcorn:false})
-        }
-        else if (text === "PO") {
-            const updated = foods.filter(item => item.is_popcorn === true)
-            setFliteredFood(updated)
-            setActive({...active, combo: false, all:false, snacks:false, beverages:false, popcorn:true})
+        let updated = foods;
+        let newActive = { ...init };
+
+        if (text === 'CO') {
+            updated = foods.filter((item) => item.is_combo);
+            newActive = { ...newActive, combo: true };
+        } else if (text === 'SN') {
+            updated = foods.filter((item) => item.is_coke);
+            newActive = { ...newActive, snacks: true };
+        } else if (text === 'BE') {
+            updated = foods.filter((item) => item.is_beverage); // Assumed is_beverage exists
+            newActive = { ...newActive, beverages: true };
+        } else if (text === 'PO') {
+            updated = foods.filter((item) => item.is_popcorn);
+            newActive = { ...newActive, popcorn: true };
         } else {
-            setFliteredFood(foods)
-            setActive({...active, combo: false, all:true, snacks:false, beverages:false, popcorn:false})
+            newActive = { ...newActive, all: true };
         }
-    }
+
+        setFilteredFood(updated);
+        setActive(newActive);
+    };
 
     const handleCount = (id, val) => {
-        if (selectedFood.length < 5) {
-            let selected = filteredFood.map(item => item._id === id?{...item, count: item.count + val}: item)
-            setFliteredFood(selected)
-        } else {
-            alert("Only 5 items per user")
+        if (selectedFood.length >= 5 && val > 0) {
+            alert('Only 5 items per user');
+            return;
         }
-    }
+        const updated = filteredFood.map((item) =>
+            item._id === id
+                ? { ...item, count: Math.max(0, item.count + val) }
+                : item
+        );
+        setFilteredFood(updated);
+    };
 
-    React.useEffect(() => {
-        const temp = filteredFood.filter(item => item.count > 0)
-        setSelectedFood(temp)
-        dispatch(storeSelectedFood(temp))
-    }, [filteredFood])
-
-    React.useEffect(() => {
-        dispatch(storeSelectedFood(selectedFood))
-    }, [selectedFood])
-    
-    console.log(selectedFood)
-    
     return (
         <div className={styles.container}>
-            <img src="https://in.bmscdn.com/bmsin/fnb/offerbanner/web/web-offerbanner.jpg" alt="banner"/>
+            <img
+                src="https://in.bmscdn.com/bmsin/fnb/offerbanner/web/web-offerbanner.jpg"
+                alt="banner"
+            />
             <div className={styles.wrapper}>
-                <div>Grab a <a href="">bite!</a></div>
-                <span className={styles.span}>Prebook Your Meal and<span> Save More!</span></span>
+                <div>
+                    Grab a <a href="/offers">bite!</a>
+                </div>
+                <span className={styles.span}>
+                    Prebook Your Meal and<span> Save More!</span>
+                </span>
                 <div className={styles.filters}>
-                    <span style={active.all ?{color:'white', background:'#F84464', border:'none'}: {}} onClick={()=>handleFilter('ALL')}>ALL</span>
-                    <span style={active.combo ?{color:'white', background:'#F84464', border:'none'}: {}} onClick={()=>handleFilter('CO')}>COMBOS</span>
-                    <span style={active.snacks ?{color:'white', background:'#F84464', border:'none'}: {}} onClick={()=>handleFilter('SN')}>SNACKS</span>
-                    <span style={active.beverages ?{color:'white', background:'#F84464', border:'none'}: {}} onClick={()=>handleFilter('BE')}>BEVERAGES</span>
-                    <span style={active.popcorn ?{color:'white', background:'#F84464', border:'none'}: {}} onClick={() =>handleFilter('PO')}>POPCORN</span>
+                    <span
+                        style={
+                            active.all
+                                ? { color: 'white', background: '#F84464', border: 'none' }
+                                : {}
+                        }
+                        onClick={() => handleFilter('ALL')}
+                    >
+                        ALL
+                    </span>
+                    <span
+                        style={
+                            active.combo
+                                ? { color: 'white', background: '#F84464', border: 'none' }
+                                : {}
+                        }
+                        onClick={() => handleFilter('CO')}
+                    >
+                        COMBOS
+                    </span>
+                    <span
+                        style={
+                            active.snacks
+                                ? { color: 'white', background: '#F84464', border: 'none' }
+                                : {}
+                        }
+                        onClick={() => handleFilter('SN')}
+                    >
+                        SNACKS
+                    </span>
+                    <span
+                        style={
+                            active.beverages
+                                ? { color: 'white', background: '#F84464', border: 'none' }
+                                : {}
+                        }
+                        onClick={() => handleFilter('BE')}
+                    >
+                        BEVERAGES
+                    </span>
+                    <span
+                        style={
+                            active.popcorn
+                                ? { color: 'white', background: '#F84464', border: 'none' }
+                                : {}
+                        }
+                        onClick={() => handleFilter('PO')}
+                    >
+                        POPCORN
+                    </span>
                 </div>
                 <div className={styles.cards}>
-                    {filteredFood?.map(item => (
-                        <FoodCard {...item} handleCount={handleCount}
-                        // handleCounter={handleCounter}
+                    {filteredFood?.map((item) => (
+                        <FoodCard
+                            key={item._id}
+                            {...item}
+                            handleCount={handleCount}
                         />
                     ))}
                 </div>
             </div>
-            
         </div>
-    )
-}
+    );
+};
 
-export default Food
+export default Food;
